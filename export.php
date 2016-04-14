e319c43a1def7d6f5bc07459f3ee5decd2421d1d
张俊帆 <zhangjunfan@qianshengqian.com>
2016-04-14 14:23:09 +0800

    书籍名单脚本

---
+++ export.php
@@ -0,0 +1,13 @@
+<?php
+$file = fopen('test.txt','r') or exit('unable to open file');
+$all_array = array();
+while(!feof($file)){
+	$array = split(' ',fgets($file));
+	if(count($array)!=1){
+		$all_array[] = $array;
+	}
+}
+foreach($all_array as $key => &$value){
+	$value[0] = split('、',$value[0])[1];
+}
+var_dump($all_array);
