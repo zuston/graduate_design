c47b92181147b3042df01d31fb283318c5242354
zuston.shacha <zuston.shacha@gmail.com>
2016-04-17 23:44:40 +0800

    测试数据填充api完成

--- index.php
+++ index.php
@@ -84,6 +84,11 @@ Flight::route('/login/user', function(){
     }
 });
 
+
+
+
+//===============================================================
+//以下为数据填充测试api
 /**
  *xunsearch索引数据导入
  */
@@ -106,23 +111,55 @@ Flight::route('/api/xunsearch/import',function(){
     echo 2222;exit;
 });
 
-Flight::route('/api/db/import',function(){
+Flight::route('/api/db/import/user',function(){
     $userModel = new userModel();
-    $name_1 = array('殷','丁','马','黄','姜','仇','李','施','魏','许','吴','刘','周','郑');
-    $name_2 = array('高','子','英','妮','豪','杰','华','帆','樊','德','级','导','硕','博','本','科','科','乐','肋','撒','但','珀');
-    $name_3 = array('需','和','杰','空','看','心','信','新','路','有','录','维','为','请','落','魄','力','利','期','器','冰','宾','林','琳','临');
+    $name_1 = array('殷','丁','马','黄','姜','仇','李','施','魏','许','吴','刘','周','郑','董','欧阳','慕容');
+    $name_2 = array('俊','军','英','妮','豪','杰','华','帆','樊','德','美','萍','硕','博','本','华','科','乐','敏','丹','君','超','朝');
+    $name_3 = array('犇','向','杰','权','圆','欣','冉','新','洋','阳','桐','彤','志','伊','子','康','颖','黎','晨','冰','宾','林','琳','临');
 
-    $userModel -> user_name = $name_1[rand(0,count($name_1)-1)].$name_1[rand(0,count($name_2)-1)];
-    $userModel -> user_email = 'jinxi32@163.com';
-    $userModel -> user_password = 'shacha123';
-    $userModel -> user_age = 24;
-    $userModel -> user_sex = 1;
-    $userModel -> user_class = 71;
-    $userModel -> user_type = 1 ;
-    $userModel -> user_major = 1 ;
-    $userModel -> user_grade = 2012;
-    $userModel -> user_academy = 1;
-    $userModel -> insert();
+//    $userModel -> user_name = $name_1[rand(0,count($name_1)-1)].$name_1[rand(0,count($name_2)-1)];
+//    $userModel -> user_email = 'jinxi32@163.com';
+//    $userModel -> user_password = 'shacha123';
+//    $userModel -> user_age = 24;
+//    $userModel -> user_sex = 1;
+//    $userModel -> user_class = 71;
+//    $userModel -> user_type = 1 ;
+//    $userModel -> user_major = 1 ;
+//    $userModel -> user_grade = 2012;
+//    $userModel -> user_academy = 1;
+//    $userModel -> insert();
+    $sql = '';
+    for($i=0;$i<50;$i++) {
+        $name_rand_3 = rand(0,1)?'"':$name_3[rand(0,count($name_3)-1)].'"';
+        $user_name = '"' . $name_1[rand(0, count($name_1) - 1)] . $name_2[rand(0, count($name_2) - 1)].$name_rand_3;
+        $user_grade = rand(2012,2015);
+        $user_academy = rand(1,10);
+        $user_major = rand(1,20);
+        $user_type = 1;
+        $user_sex = rand(0,1);
+        $user_class = rand(1,9).rand(1,2);
+        $user_age = 24-($user_grade-2012);
+//        $db .= '('.$user_name.',"jinxi32@163.com","shacha123",24,'.$user_sex.','$user_class',1,{$user_major},{$user_grade},{$user_academy}),';
+//        $db .= '('.$user_name.',jinxi32@163.com,shacha123,24,'.$user_sex.','.$user_class.','.$user_type.','.$user_major.','.$user_grade.','.$user_academy.'),';
+        $sql .=sprintf('(%s,"jinxi32@163.com","shacha123",%d,%d,%d,%d,%d,%d,%d),',$user_name,$user_age,$user_sex,$user_class,$user_type,$user_major,$user_grade,$user_academy);
+    }
+    $sql_prefix ='insert into user(user_name,user_email,user_password,user_age,user_sex,user_class,user_type,user_major,user_grade,user_academy) values ';
+    echo $sql_prefix.$sql;
+
+
+});
+
+Flight::route('/api/db/import/user_relation',function(){
+    $sql = '';
+    for($i=0;$i<2000;$i++){
+        $user_id = rand(1,806);
+        $book_id = rand(1,824);
+        $ubr_due_at = '"2016-05-20 00:00:00"';
+        $ubr_absorted = rand(0,1);
+        $sql .= sprintf('(%d,%d,%s,%d),',$user_id,$book_id,$ubr_due_at,$ubr_absorted);
+    }
+    $sql = 'insert into user_book_relation(user_id,book_id,ubr_due_at,ubr_absorted) values'.$sql;
+    echo $sql;exit;
 });
 
 Flight::start();
